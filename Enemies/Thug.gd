10224f8c5f1e4f6a184a74d90a0aab773b0c3150
Nicholas Rodine <halfofastaple@gmail.com>
2019-01-08 20:45:55 -0500

    Made the thugs move towards the player

--- Enemies/Thug.gd
+++ Enemies/Thug.gd
@@ -1,4 +1,16 @@
 extends KinematicBody2D
 
+var Player = preload("res://Player/Player.tscn");
+var target = null;
+
+func _physics_process(delta):
+	if (target == null): return;
+	var dp = target.position - self.position;
+	move_and_slide(dp.normalized() * 10);
+
 func damage():
 	queue_free();
+
+func _on_DetectionRadius_body_entered(body: PhysicsBody2D):
+	if (body.is_in_group("player")):
+		target = body;

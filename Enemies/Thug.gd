191013669ff6d1f7d00f0ff48dbd90a8b2cdf13b
Nicholas Rodine <halfofastaple@gmail.com>
2019-01-19 19:17:45 -0500

    Gave all the buildings depth. Fixed textures.

--- Enemies/Thug.gd
+++ Enemies/Thug.gd
@@ -1,11 +1,12 @@
 extends KinematicBody
 
 var target = null;
+const SPEED = 2.4;
 
 func _physics_process(_delta):
 	if (target == null): return;
 	var dp = target.translation - self.translation;
-	move_and_slide(dp.normalized() * .8);
+	dp = move_and_slide(dp.normalized() * SPEED);
 
 func _process(_delta):
 	if (target == null): return;
@@ -21,6 +22,6 @@ func _process(_delta):
 func damage():
 	queue_free();
 
-func _on_DetectionRadius_body_entered(body: PhysicsBody):
+func _on_DetectionRadius_body_entered(body: Node):
 	if (body.is_in_group("player")):
 		target = body;
--- Player/Bullet.gd
+++ Player/Bullet.gd
@@ -2,9 +2,14 @@ extends Area
 
 var source = null;
 var velocity = Vector3();
+var life = 5;
 
 func _physics_process(delta):
-	self.translation += velocity * delta;
+	var dp = velocity * delta;
+	self.translation += dp;
+	self.life -= dp.length();
+	if (self.life <= 0):
+		self.queue_free();
 
 func _on_Bullet_body_entered(body):
 	if (body == source): return;
